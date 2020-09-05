62d6f482d5262bb36d38838a5d87acf26efd1d71
byuegv <byuegv@163.com>
2020-09-05 09:52:29 +0800

    expose coder-server to internet

--- rcoder/startup.sh
+++ rcoder/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # start code-server
-/opt/rcoder/bin/code-server --auth none --bind-addr "127.0.0.1:8080"
-
+/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080"
 
 # start sshd
 /usr/sbin/sshd -D
