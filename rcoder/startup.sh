d1cbc924004c2804776d91c5fce520fdd02cf7c9
scinterv <scinterv@gmail.com>
2020-09-05 12:34:04 +0800

    redirect output to /dev/null

--- rcoder/startup.sh
+++ rcoder/startup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # start code-server
-/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080"
+/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080" &> /dev/null &
 
 # start sshd
 /usr/sbin/sshd -D
-
-
