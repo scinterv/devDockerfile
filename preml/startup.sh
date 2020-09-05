52d90b4dcc71f71dcfe89948a95fc616f0ceec9e
byuegv <byuegv@163.com>
2020-09-05 12:44:07 +0800

    remove > /dev/null for redis-server

--- preml/startup.sh
+++ preml/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # start code-server
-/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080" &> dev/null &
+/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080" & > dev/null
 
 # start jupyter notebook
 jupyter-notebook --no-browser --allow-root --ip='*' --port 8888 --NotebookApp.token='' --NotebookApp.password='' &> /dev/null &
 
 # start up redis-service
-redis-server /etc/redis/redis.conf &> /dev/null &
+redis-server /etc/redis/redis.conf
 
 # start up sshd service
 /usr/sbin/sshd -D
