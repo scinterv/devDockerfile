6b21aa5e8f4ac0c0fe962f265e67f02410e08701
scinterv <scinterv@gmail.com>
2020-09-05 12:18:47 +0800

    fix startup.sh of preml, start jupyter before redis-server

--- preml/startup.sh
+++ preml/startup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# start coder-server
-/opt/rcoder/bin/coder-server --auth none --bind-addr "0.0.0.0:8080"
+# start code-server
+/opt/rcoder/bin/code-server --auth none --bind-addr "0.0.0.0:8080" & > dev/null
+
+# start jupyter notebook
+jupyter-notebook --no-browser --allow-root --ip='*' --port 8888 --NotebookApp.token='' --NotebookApp.password='' & > /dev/null
 
 # start up redis-service
 redis-server /etc/redis/redis.conf
 
 # start up sshd service
 /usr/sbin/sshd -D
-
-# start jupyter notebook
-jupyter-notebook --no-browser --allow-root --ip='*' --port 8888
---NotebookApp.token='' --NotebookApp.password='' & > /dev/null
